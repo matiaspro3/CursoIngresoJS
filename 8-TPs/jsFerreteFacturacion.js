abca28e765d4bd087642a27dccfec779fc77369a
hdillon <dillonhoraciodavid@gmail.com>
2016-08-25 21:15:26 -0300

    Primer commit

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,25 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
-}
-function Promedio () 
-{
-	
-}
-function PrecioFinal () 
-{
-	
-}
+
+var app = angular.module('ferreteFacturacion', []);
+
+app.controller('ControladorFacturacion', function($scope){
+
+$scope.test="Hola Prima de Lucas";
+
+$scope.PrecioUno="";
+$scope.PrecioDos="";
+$scope.PrecioTres="";
+$scope.Sumar=function(){
+	$scope.resultado = parseInt($scope.PrecioUno) + parseInt($scope.PrecioDos) + parseInt($scope.PrecioTres);
+
+  }
+
+  $scope.Promedio=function(){
+	$scope.resultado = (parseInt($scope.PrecioUno) + parseInt($scope.PrecioDos) + parseInt($scope.PrecioTres))/ 3;
+
+  }
+
+
+});
